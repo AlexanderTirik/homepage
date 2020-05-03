4d38bc3544b24330d6e545bd301312f623e6bfa4
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-03 22:56:02 +0300

    Added function of default growing

---
+++ assets/scripts/defaultGrow.js
@@ -0,0 +1,9 @@
+import {animateValue} from "./animateValue.js"
+import {randomInteger} from "./randomInteger.js"
+
+setInterval(() => {
+  var shares = document.getElementById("shares").innerHTML
+  shares = +shares.slice(0, -1)
+  var range = Math.floor(shares*(randomInteger(5,10)/100))
+  if (shares < 10000) animateValue("shares", range, 500)
+}, randomInteger(3000,6000))
