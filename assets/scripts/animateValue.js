5aae73e854b2400144bc64a97c93f5561985761f
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-03 22:29:13 +0300

    Added function for animation changing

---
+++ assets/scripts/animateValue.js
@@ -0,0 +1,17 @@
+function animateValue(id, range, duration) {
+  var current = document.getElementById(id).innerHTML
+  current = +current.slice(0, -1)
+  var start = current
+  var increment = range > 0 ? 1 : -1
+  var stepTime = Math.abs(Math.floor(duration / range))
+  var obj = document.getElementById(id)
+  var timer = setInterval(function () {
+    current += increment
+    obj.innerHTML = current + "$"
+    if (current == start + range) {
+      clearInterval(timer)
+    }
+  }, stepTime)
+}
+
+export { animateValue }
