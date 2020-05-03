263979d1882c1c2d2b8a2848d37c9adca513db0e
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-03 23:52:07 +0300

    Reduced limit of growing

--- assets/scripts/defaultGrow.js
+++ assets/scripts/defaultGrow.js
@@ -5,5 +5,5 @@ setInterval(() => {
   var shares = document.getElementById("shares").innerHTML
   shares = +shares.slice(0, -1)
   var range = Math.floor(shares*(randomInteger(5,10)/100))
-  if (shares < 10000) animateValue("shares", range, 500)
+  if (shares < 2000) animateValue("shares", range, 500)
 }, randomInteger(3000,6000))
